2a61c05b09c4646f57493c6d0a0ce3ede1680482
Ofekst <ofekstreicher0@gmail.com>
2025-04-17 13:14:16 +0300

    Finished Message class with all requested constructors

--- test/Message.java
+++ test/Message.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 public class Message {
@@ -8,5 +9,51 @@ public class Message {
     public final double asDouble;
     public final Date date;
 
+    /**
+     * This is our main constructor
+     *
+     * @param data - Message data in bytes
+     */
+    public Message(byte[] data) {
+        this.data = data;
+        this.asText = new String(data, StandardCharsets.UTF_8);
+        this.asDouble = convertToDouble(this.asText);
+        this.date = new Date();
+    }
+
+    /**
+     * Constructor get String parameter
+     * and calls our main constructor
+     *
+     * @param asText - Message data as string
+     */
+    public Message(String asText) {
+        this(asText.getBytes());
+    }
+
+    /**
+     * Constructor get Double parameter
+     * and calls our main constructor
+     *
+     * @param asDouble - Message data as double
+     */
+    public Message(Double asDouble) {
+        this(Double.toString(asDouble));
+    }
+
+    // Extra functions
+
+    /**
+     * The function gets data as String
+     * and try to convert it into Double
+     * if can't return NaN
+     */
+    private double convertToDouble(String data) {
+        try {
+            return Double.parseDouble(data);
+        } catch (NumberFormatException e) {
+            return Double.NaN;
+        }
+    }
 
 }

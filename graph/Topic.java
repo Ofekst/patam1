f4b1b4da95c3c35e9212e24aabdde6e846ba99c1
Ofekst <ofekstreicher0@gmail.com>
2025-04-17 13:37:41 +0300

    Created Topic class with all requested methods.

--- graph/Topic.java
+++ graph/Topic.java
@@ -1,24 +1,93 @@
 package graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class represents a Topic in a publish-subscribe system.
+ * A Topic can have multiple publishers and subscribers.
+ * Publishers can send messages to the topic, and subscribers
+ * receive those messages via a callback.
+ */
+
 public class Topic {
     public final String name;
-    Topic(String name){
-        this.name=name;
+    private List<Agent> subs;
+    private List<Agent> pubs;
+
+    /**
+     * Constructs a Topic with the specified name.
+     *
+     * @param name the name of the topic
+     */
+    Topic(String name) {
+        this.name = name;
+        this.subs = new ArrayList<>();
+        this.pubs = new ArrayList<>();
     }
 
-    public void subscribe(Agent a){
-    }
-    public void unsubscribe(Agent a){
-    }
-
-    public void publish(Message m){
-    }
-
-    public void addPublisher(Agent a){
+    /**
+     * Subscribes an agent to this topic.
+     *
+     * @param a the agent to subscribe
+     */
+    public void subscribe(Agent a) {
+        this.subs.add(a);
     }
 
-    public void removePublisher(Agent a){
+    /**
+     * Unsubscribes an agent from this topic.
+     *
+     * @param a the agent to unsubscribe
+     */
+    public void unsubscribe(Agent a) {
+        this.subs.remove(a);
     }
 
+    /**
+     * Publishes a message to all subscribers of this topic.
+     * Each subscriber receives the message through its callback method.
+     *
+     * @param m the message to publish
+     */
+    public void publish(Message m) {
+        subs.parallelStream().forEach(a -> a.callback(this.name, m));
+    }
+
+    /**
+     * Registers an agent as a publisher to this topic.
+     *
+     * @param a the agent to add as a publisher
+     */
+    public void addPublisher(Agent a) {
+        this.pubs.add(a);
+    }
+
+    /**
+     * Removes an agent from the list of publishers of this topic.
+     *
+     * @param a the agent to remove
+     */
+    public void removePublisher(Agent a) {
+        this.pubs.remove(a);
+    }
+
+    /**
+     * Returns the list of subscribers for this topic.
+     *
+     * @return a list of subscribed agents
+     */
+    public List<Agent> getSubs() {
+        return subs;
+    }
+
+    /**
+     * Returns the list of publishers for this topic.
+     *
+     * @return a list of publishing agents
+     */
+    public List<Agent> getPubs() {
+        return pubs;
+    }
 
 }

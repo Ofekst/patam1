77b9de2de0427c69db39deb0d4f425e3758f0a17
Ofekst <Ofekstrteicher0@gmail.com>
2025-05-15 19:49:45 +0300

    Created constructor, getters, setters and addEdge functions

--- graph/Node.java
+++ graph/Node.java
@@ -9,6 +9,58 @@ public class Node {
     private List<Node> edges;
     private Message msg;
 
+    /**
+     * Constructs a Node with the specified name.
+     *
+     * @param node_name the name of the node
+     */
+    public Node (String node_name){
+        this.name=node_name;
+        this.edges = new ArrayList<Node>();
+        this.msg=null;
+    }
 
+
+    // Getter functions
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Node> getEdges() {
+        return edges;
+    }
+
+    public Message getMsg() {
+        return msg;
+    }
+
+    // Setter functions
+
+    public void setName(String node_name) {
+        this.name = node_name;
+    }
+
+    public void setEdges(List<Node> edges) {
+        this.edges = edges;
+    }
+
+    public void setMsg(Message msg) {
+        this.msg = msg;
+    }
+
+    // Extra functions
+
+    /**
+     * Add new node to the list (if it does not exist).
+     *
+     * @param node the node to add to our list
+     */
+    public void addEdge(Node node){
+        if (!edges.contains(node)){
+            this.edges.add(node);
+        }
+    }
+    //TODO: Need to Add the function hasCycles()
 
 }

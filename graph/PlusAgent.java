630b9712fe7d4bafb8076f6b2604fe7ae4bd67d6
Ofekst <Ofekstrteicher0@gmail.com>
2025-06-05 15:37:17 +0300

    Done with PlusAgent

--- graph/PlusAgent.java
+++ graph/PlusAgent.java
@@ -3,24 +3,66 @@ package graph;
 import graph.TopicManagerSingleton.TopicManager;
 
 public class PlusAgent implements Agent{
+    private final String [] subs;
+    private final String [] pubs;
 
+    private final TopicManagerSingleton.TopicManager topicManager;
+    private Double x;
+    private Double y;
+
+    public PlusAgent (String [] subs, String [] pubs){
+        this.subs = subs;
+        this.pubs = pubs;
+        this.topicManager = TopicManagerSingleton.get();
+
+        if (this.subs.length == 1){
+          this.topicManager.getTopic(this.subs[0]).subscribe(this);
+        }
+        else if (this.subs.length > 1) {
+            this.topicManager.getTopic(this.subs[0]).subscribe(this);
+            this.topicManager.getTopic(this.subs[1]).subscribe(this);
+        }
+
+        if (this.pubs.length != 0){
+            this.topicManager.getTopic(this.pubs[0]).addPublisher(this);
+        }
+    }
     @Override
     public String getName() {
-        return "";
+        return "PlusAgent";
     }
 
     @Override
     public void reset() {
-
+        this.x = Double.NaN;
+        this.y = Double.NaN;
     }
 
     @Override
     public void callback(String topic, Message msg) {
+        if (topic.equals(this.subs[0])){
+            this.x = msg.asDouble;
+        }
+        else if (subs.length > 1 && topic.equals(this.subs[1])) {
+            this.y = msg.asDouble;
+        }
 
+        if (x!= null && this.y!=null){
+            double result = x + y;
+            this.topicManager.getTopic(topic).publish(new Message(result));
+        }
     }
 
     @Override
     public void close() {
-
+    try {
+        this.topicManager.getTopic(this.subs[0]).unsubscribe(this);
+        this.topicManager.getTopic(this.subs[1]).unsubscribe(this);
+        this.topicManager.getTopic(this.pubs[0]).removePublisher(this);
+    }
+    catch (Exception e)
+    {
+        System.out.println("The error you got is: " + e.getMessage());
+    }
     }
 }

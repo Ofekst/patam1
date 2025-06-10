c5172d18880152865426b31b62c4f34a145bf4b4
Ofekst <Ofekstrteicher0@gmail.com>
2025-06-10 13:49:08 +0300

    Done with RequestParser

--- graph/RequestParser.java
+++ graph/RequestParser.java
@@ -2,14 +2,76 @@ package graph;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class RequestParser {
 
     public static RequestInfo parseRequest(BufferedReader reader) throws IOException {
-        // implement
+        String httpMethod;
+        String httpUri;
+        Map<String,String> params = new HashMap<>();
 
-        return null;
+        String requestLine = reader.readLine();
+        if (requestLine.isEmpty() || requestLine == null){
+            throw new IllegalArgumentException("The request line is invalid");
+        }
+
+        String [] requestParts = requestLine.split(" ");
+        if (requestParts.length < 2){
+            throw new IllegalArgumentException("Invalid request line");
+        }
+
+        httpMethod = requestParts[0];
+        httpUri = requestParts[1];
+
+        String [] httpUriParts = httpUri.split("\\?");
+        String [] httpUriSegments = httpUriParts[0].split("/");
+        httpUriSegments = Arrays.stream(httpUriSegments).filter(
+                segment -> !segment.isEmpty()).toArray(String[]::new);
+
+        if(httpUriParts.length > 1){
+            String queryString = httpUriParts[1];
+            String [] queryParams = queryString.split("&");
+            for (String param : queryParams){
+                String [] keyValue = param.split("=");
+                if (keyValue.length == 2){
+                    params.put(keyValue[0],keyValue[1]);
+                }
+            }
+        }
+
+        String line ;
+        int contentLength = 0;
+        while ((line = reader.readLine()) != null && !line.isEmpty()){
+            if(line.startsWith("Content-Length:")){
+                contentLength = Integer.parseInt(line.split(":")[1].trim());
+            }
+        }
+
+        if (contentLength > 0){
+            String paramLine = reader.readLine();
+            if (paramLine!= null && paramLine.contains("=")){
+                String [] paramParts = paramLine.split("=");
+                if (paramParts.length == 2){
+                    params.put(paramParts[0],paramParts[1]);
+                }
+            }
+            reader.readLine();
+
+            String contentLine = reader.readLine();
+
+            if (contentLine != null) {
+                String finalContent = contentLine + "\n";
+                byte[] contentBytes = finalContent.getBytes();
+                return new RequestInfo(httpMethod, httpUri, httpUriSegments, params, contentBytes);
+            }
+
+        }
+
+        return new RequestInfo(httpMethod, httpUri, httpUriSegments, params, new byte[0]);
+
     }
 
     // RequestInfo given internal class
